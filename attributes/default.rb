460cf7668e267aeaf554a12b1601dc0fc1f343c4
Mahmoud Ismail <maism@hops.io>
2016-06-15 14:42:12 +0200

    update ePipe version to 0.2.1

--- attributes/default.rb
+++ attributes/default.rb
@@ -5,7 +5,7 @@ include_attribute "ndb"
 include_attribute "elastic"
 include_attribute "elasticsearch"
 
-default.epipe.version                  = "0.1.1"
+default.epipe.version                  = "0.2.1"
 default.epipe.user                     = node.apache_hadoop.hdfs.user
 default.epipe.group                    = node.apache_hadoop.group
 default.epipe.url                      = "#{node.download_url}/epipe/#{node.platform_family}/epipe-#{node.epipe.version}.tar.gz"

eaad23d2c5524445b9f31462929a08ee531c2953
Jim Dowling <jdowling@sics.se>
2016-06-23 00:38:23 +0200

    create user, re-start  on failure

--- recipes/install.rb
+++ recipes/install.rb
@@ -1,3 +1,21 @@
+
+user node.epipe.user do
+  supports :manage_home => true
+  home "/home/#{node.epipe.user}"
+  action :create
+  system true
+  shell "/bin/bash"
+  not_if "getent passwd #{node.epipe.user}"
+end
+
+group node.epipe.group do
+  action :modify
+   members ["#{node.epipe.user}"]
+  append true
+end
+
+
+
 package_url = "#{node.epipe.url}"
 base_package_filename = File.basename(package_url)
 cached_package_filename = "/tmp/#{base_package_filename}"

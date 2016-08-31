6c98f88979e658076dfca3627922a59bdb94ebe7
Jim Dowling <jdowling@sics.se>
2016-09-01 00:09:35 +0200

    new supermarket release

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ name             "epipe"
 license          "Apache v2.0"
 description      "Installs/Configures a HopsFS to ElasticSearch connector"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.0"
+version          "0.1.1"
 source_url       "https://github.com/hopshadoop/epipe-chef"
 
 %w{ ubuntu debian centos }.each do |os|
@@ -17,11 +17,31 @@ depends 'apache_hadoop'
 depends 'hops'
 depends 'elastic'
 
-recipe "epipe::default", "Installs and configures Epipe Server"
+recipe "epipe::install", "Installs Epipe Server"
+recipe "epipe::default", "configures Epipe Server"
 recipe "epipe::purge", "Deletes the Epipe Server"
 
 attribute "epipe/user",
-:description => "User to run Epipe server as",
-:type => "string"
+          :description => "User to run Epipe server as",
+          :type => "string"
 
+attribute "epipe/group",
+          :description => "Group to run Epipe server as",
+          :type => "string"
+
+attribute "epipe/version",
+          :description => "Version of epipe to use",
+          :type => "string"
+
+attribute "epipe/url",
+          :description => "Url to epipe binaries",
+          :type => "string"
+
+attribute "epipe/dir",
+          :description => "Parent directory to install epipe in (/srv is default)",
+          :type => "string"
+
+attribute "epipe/pid_file",
+          :description => "Change the location for the pid_file.",
+          :type => "string"
 

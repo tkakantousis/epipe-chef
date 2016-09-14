c375a64f949e1ea3ccf8722e5804e3b7eaf30518
Jim Dowling <jdowling@sics.se>
2016-09-14 13:38:58 +0200

    all ip stuff to default recipe

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,6 +1,75 @@
 nmy_ip = my_private_ip()
 my_public_ip = my_public_ip()
 
+
+nn = private_recipe_ip("apache_hadoop", "nn") + ":#{node.apache_hadoop.nn.port}"
+elastic = private_recipe_ip("elastic", "default") + ":#{node.elastic.port}"
+
+epipe_downloaded = "#{node.epipe.base_dir}/.epipe.extracted_#{node.epipe.version}"
+# Extract epipe
+bash 'extract_epipe' do
+        user "root"
+        code <<-EOH
+                tar -xf #{cached_package_filename} -C #{node.epipe.dir}
+                chown -R #{node.epipe.user}:#{node.epipe.group} #{node.epipe.base_dir}
+                cd #{node.epipe.base_dir}
+                touch #{epipe_downloaded}
+                chown #{node.epipe.user} #{epipe_downloaded}
+        EOH
+     not_if { ::File.exists?( epipe_downloaded ) }
+end
+
+file node.epipe.home do
+  action :delete
+  force_unlink true  
+end
+
+link node.epipe.home do
+  owner node.epipe.user
+  group node.epipe.group
+  to node.epipe.base_dir
+end
+
+
+# file "#{node.epipe.home}/conf/epipe-site.xml" do
+#   action :delete
+# end
+
+# private_ip = my_private_ip()
+
+# template"#{node.epipe.home}/conf/epipe-site.xml" do
+#   source "epipe-site.xml.erb"
+#   owner node.epipe.user
+#   group node.epipe.group
+#   mode 0655
+#   variables({ 
+#            })
+# end
+
+ndb_connectstring()
+
+template"#{node.epipe.home}/bin/start-epipe.sh" do
+  source "start-epipe.sh.erb"
+  owner node.epipe.user
+  group node.epipe.group
+  mode 0750
+   variables({ :ndb_connectstring => node.ndb.connectstring,
+               :database => "hops",
+               :meta_database => "hopsworks",
+               :elastic_addr => elastic,
+            })
+end
+
+template"#{node.epipe.home}/bin/stop-epipe.sh" do
+  source "stop-epipe.sh.erb"
+  owner node.epipe.user
+  group node.epipe.group
+  mode 0750
+end
+
+
+
+
 case node.platform
 when "ubuntu"
  if node.platform_version.to_f <= 14.04

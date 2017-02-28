6cb98ec7bf4146cc2b3f0f8e80ede352428c65a5
Jim Dowling <jdowling@sics.se>
2017-02-28 12:18:38 -0800

    removing apache_hadoop dependency

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,13 +1,13 @@
 include_attribute "kagent"
-include_attribute "apache_hadoop"
+include_attribute "hops"
 include_attribute "hops"
 include_attribute "ndb"
 include_attribute "elastic"
 include_attribute "elasticsearch"
 
 default.epipe.version                  = "0.4.0"
-default.epipe.user                     = node.apache_hadoop.hdfs.user
-default.epipe.group                    = node.apache_hadoop.group
+default.epipe.user                     = node.hops.hdfs.user
+default.epipe.group                    = node.hops.group
 default.epipe.url                      = "#{node.download_url}/epipe/#{node.platform_family}/epipe-#{node.epipe.version}.tar.gz"
 default.epipe.systemd                  = "true"
 default.epipe.dir                      = "/srv"

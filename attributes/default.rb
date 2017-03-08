72e7372e0a34c7dc0d7837cabced38f72bbab945
Jim Dowling <jdowling@sics.se>
2017-03-08 15:53:29 +0100

    install/user now a global attr

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,13 +1,12 @@
 include_attribute "kagent"
-include_attribute "apache_hadoop"
 include_attribute "hops"
 include_attribute "ndb"
 include_attribute "elastic"
 include_attribute "elasticsearch"
 
 default.epipe.version                  = "0.4.0"
-default.epipe.user                     = node.apache_hadoop.hdfs.user
-default.epipe.group                    = node.apache_hadoop.group
+default.epipe.user                     = node.install.user.empty? ? node.hops.hdfs.user : node.install.user
+default.epipe.group                    = node.install.user.empty? ? node.hops.group : node.install.user
 default.epipe.url                      = "#{node.download_url}/epipe/#{node.platform_family}/epipe-#{node.epipe.version}.tar.gz"
 default.epipe.systemd                  = "true"
 default.epipe.dir                      = node.install.dir.empty? ? "/srv" : node.install.dir

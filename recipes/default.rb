09a69c499bd9b0c1854816bdaee1b392403690d0
Theofilos Kakantousis <t.kakantousis@gmail.com>
2018-10-29 11:53:51 +0100

    reindex

--- recipes/default.rb
+++ recipes/default.rb
@@ -36,6 +36,18 @@ template"#{node['epipe']['base_dir']}/bin/start-epipe.sh" do
             })
 end
 
+template"#{node['epipe']['base_dir']}/bin/reindex-epipe.sh" do
+  source "reindex-epipe.sh.erb"
+  owner node['epipe']['user']
+  group node['epipe']['group']
+  mode 0750
+   variables({ :ndb_connectstring => node['ndb']['connectstring'],
+               :database => "hops",
+               :meta_database => "hopsworks",
+               :elastic_addr => elastic,
+            })
+end
+
 template"#{node['epipe']['base_dir']}/bin/stop-epipe.sh" do
   source "stop-epipe.sh.erb"
   owner node['epipe']['user']
@@ -43,7 +55,13 @@ template"#{node['epipe']['base_dir']}/bin/stop-epipe.sh" do
   mode 0750
 end
 
-
+#when upgrading, we want to reindex the newly created projects index
+bash 'reindex epipe' do
+  user node['epipe']['user']
+  code <<-EOF
+     #{node['epipe']['base_dir']}/bin/reindex-epipe.sh
+  EOF
+end
 
 
 case node['platform']
